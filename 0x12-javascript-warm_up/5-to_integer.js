c51e0144017729d4dd8fdfd6c6917121f37c3dd0
-Brownlolaoo <-aramyeedey199@yahoo.com>
2022-10-03 21:25:54 +0800

    intrger

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-let arg = process.argv[2];
-if (isNAN(arg)) {
-  console.log('Not s number');
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
 } else {
-  console.log('My number: ' + arg);
+  console.log('My number:', parseInt(process.argv[2]));
 }
