1bff84cd26dc2680b32d7356d517edc50027a5c8
-Brownlolaoo <-aramyeedey199@yahoo.com>
2022-10-03 16:25:13 +0800

    adding argv

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let p = process.argv.lenght;
+if (p === 2) {
+console.log("No Argument");
+} else if (p === 3) {
+console.log("Argument found");
+} else {
+console.log("Arguments found");
+}
