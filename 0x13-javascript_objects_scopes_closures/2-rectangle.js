3c7ac44ff45d66dc329ba92b8e92341aad084165
-Brownlolaoo <-aramyeedey199@yahoo.com>
2022-10-04 17:50:08 +0800

    javascriptOBJ

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
     constructor (w, h) {
-        if (w > 0 && h > 0) {
+        if (((w = parseint(w)) > 0) && ((h = parseint(h)) > 0)) {
             this.width = w;
             this.height = h;
         }
     }
 }
-module.export = Rectangle;
+module.exports = Rectangle;
