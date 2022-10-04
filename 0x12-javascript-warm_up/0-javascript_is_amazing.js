65bc6cca0c9ff7a4815ff8bd75913782cc4aaa76
-Brownlolaoo <-aramyeedey199@yahoo.com>
2022-10-04 17:05:53 +0800

    initial commit

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,15 @@
-#!/usr/bin/node
-const myVar = "javascript is amazing";
-console.log(myVar);
+//let b = 1;
+//function myfunction(a) {
+    //console.log(a + b);
+    //b = a;
+//}
+
+//myfunction(3);
+//myfunction(4);
+
+function myfunction(a) {
+    console.log(a);
+}
+
+const number = 12;
+myfunction(number);
