b691ccb5323a3e4c26ebec60dd06890db6214cfd
-Brownlolaoo <-aramyeedey199@yahoo.com>
2022-10-04 21:24:58 +0800

    javascriptOBJ

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+    const arr = [];
+    for (let p = list.length - 1; i >= 0; p--) {
+      arr.push(list[p]);
+    }
+    return (arr);
+  };
